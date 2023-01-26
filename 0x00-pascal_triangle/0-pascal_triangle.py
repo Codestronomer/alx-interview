ebad4db6cadb5540506977962edf438223b9c5da
codestronomer <johnrumide6@gmail.com>
2023-01-26 15:40:58 -0800

    [fix] syntax errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,7 @@ Module contains a list of lists of integers representing
 the pascal's triangle of integer n
 """
 
+
 def pascal_triangle(n):
     """
     function returns a list of lists of integers
@@ -16,8 +17,8 @@ def pascal_triangle(n):
     for i in range(0, n + 1):
         c = 1
         arr2 = []
-        for j in range(1, i+1):
+        for j in range(1, i + 1):
             arr2.append(c)
-            c = c * (i -j) // j
+            c = c * (i - j) // j
         arr.append(arr2)
     return arr[1:]
