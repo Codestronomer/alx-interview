192b2305918c19be4f7da80d7c907bd17ca514c8
codestronomer <johnrumide6@gmail.com>
2023-01-26 15:27:28 -0800

    [fix] logic in 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,7 +12,8 @@ def pascal_triangle(n):
     for i in range(0, n + 1):
         c = 1
         arr2 = []
-        for i in range(1, i+1):
-            arr2.append(i)
+        for j in range(1, i+1):
+            arr2.append(c)
+            c = c * (i -j) // j
         arr.append(arr2)
     return arr
