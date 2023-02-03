e3526b6decd8a15b898efb41188a19aad6834e15
codestronomer <johnrumide6@gmail.com>
2023-02-03 01:55:07 +0100

    [fix] logic

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -28,7 +28,7 @@ def canUnlockAll(boxes):
 
     # Iterate map and open boxes
     i = len(boxes)
-    for i in range(1, len(boxes)):
+    for i in range(1, len(boxes) - 1):
         for box in map.values():
             if box['open']:
                 # Open all boxes that box holds key for
