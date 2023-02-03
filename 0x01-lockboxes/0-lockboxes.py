84d4207d932eb205113a8e286bdcd3a973d061a5
codestronomer <johnrumide6@gmail.com>
2023-02-03 01:45:08 +0100

    [fix] logic

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
-
-
 """
-Module: 0-Lockboxes.py
-Problem: You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 and
-each box may contain keys to the other boxes.
-Write a method that determines if all the boxes can be opened.
-
-- Prototype: def canUnlockAll(boxes)
-- boxes is a list of lists
-- A key with the same number as a box opens that box
-- You can assume all keys will be positive integers
-- There can be keys that do not have boxes
-- The first box boxes[0] is unlocked
-- Return True if all boxes can be opened, else return False
+Defines the canUnlockAll function
 """
 
 
 def canUnlockAll(boxes):
     """
-    Traverses list and returns True if all boxes in the list
-    are opened otherwise, returns False.
+    Checks if all boxes can be unlocked
+    Args:
+        boxes (list): list of boxes to check
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise
     """
-    if not boxes or type(boxes) is not list:
-        return False
-    if (len(boxes)) == 0:
-        return False
+    # Create map
+    map = {}
+    for i, box in enumerate(boxes):
+        if i == 0:
+            map[i] = {
+                "open": True,
+                "items": box
+            }
+        else:
+            map[i] = {
+                "open": False,
+                "items": box
+            }
 
-    for i in range(1, len(boxes) - 1):
-        unlocked = False
-        for j in range(len(boxes)):
-            if i in boxes[j] and j != i:
-                unlocked = True
-                break
-        if not unlocked:
+    # Iterate map and open boxes
+    i = len(boxes)
+    while i > 0:
+        for box in map.values():
+            if box['open']:
+                # Open all boxes that box holds key for
+                for item in box['items']:
+                    if item <= len(boxes) and item >= 0:
+                        map[item]['open'] = True
+        i -= 1
+
+    # Check if all boxes were opened
+    for box in map.values():
+        if not box['open']:
             return False
     return True
