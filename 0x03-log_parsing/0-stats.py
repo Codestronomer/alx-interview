6a3fd076182c636981fc426f8afb409fa92b8cc3
John Rumide <johnrumide6@gmail.com>
2023-02-17 00:20:50 +0100

    [fix] 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,34 +6,41 @@ This module parses logs
 import sys
 import re
 
-line_count = 0
+
 total_size = 0
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                 '403': 0, '404': 0, '405': 0, '500': 0}
 regex = re.compile(r"^([0-9]*\.){3}\d* - \[(\d+-){2}\d+ "
                    r"(\d+:){2}\d+.\d+] \"\w+ /\w+/\d+ \w+/\d.\d\" \d+ \d+"
                    )
+
+
+def printstats():
+    """
+    prints the required statistics gotten from the logs
+    Args:
+        None
+    Return:
+        None
+    """
+    print("File size: {}".format(total_size))
+    for code, count in sorted(status_codes.items()):
+        if count != 0:
+            print("{}: {}".format(code, count))
+
+
 if __name__ == "__main__":
     try:
-        for line in sys.stdin:
-            if 'EXIT' == line.rstrip() or line_count == 10:
-                print("File size: {}".format(total_size))
-                for code, count in sorted(status_codes.items()):
-                    if count != 0:
-                        print("{}: {}".format(code, count))
-                line_count = 0
-                total_size = 0
-                status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                                '403': 0, '404': 0, '405': 0, '500': 0}
+        for i, line in enumerate(sys.stdin, start=1):
             if not re.match(regex, line):
                 continue
-            values = line.split()
+            values = line.rstrip().split()
             total_size += int(values[-1])
             status_codes[values[-2]] += 1
-            line_count += 1
+
+            if i % 10 == 0:
+                printstats()
+        printstats()
     except KeyboardInterrupt:
-        print("File size: {}".format(total_size))
-        for code, count in sorted(status_codes.items()):
-            if count != 0:
-                print("{}: {}".format(code, count))
+        printstats()
         raise
