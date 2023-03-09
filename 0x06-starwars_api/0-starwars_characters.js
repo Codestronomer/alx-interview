bad67a947ae705d405196f84e810769be6adadee
codestronomer <johnrumide6@gmail.com>
2023-03-09 01:59:56 +0100

    [fix] semistandard syntax errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints the characters from a starwars movie passed from cmd line
-let request = require('request')
-const promisify = require('util').promisify
+let request = require('request');
+const promisify = require('util').promisify;
 
 // turn module to a promise object
 request = promisify(request);
@@ -9,16 +9,22 @@ request = promisify(request);
 const num = process.argv[2];
 
 const getChars = async () => {
-	const res = await request(`https://swapi-api.alx-tools.com/api/films/${num}`);
-	if (res.statusCode == "200") {
-		const chars = JSON.parse(res.body).characters;
-		chars.forEach(getname)
-	}
-}
+  try {
+    const res = await request(`https://swapi-api.alx-tools.com/api/films/${num}`);
+    const chars = JSON.parse(res.body).characters;
+    chars.forEach(getname);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getname = async (character) => {
-	const res = await request(character);
-	console.log(JSON.parse(res.body).name);
-}
+  try {
+    const res = await request(character);
+    console.log(JSON.parse(res.body).name);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 getChars();
